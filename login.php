1ce7edab062955498e4ef7fb76a1b89382a1d1d1
NickDabizaz <nicklausdabizaz12@gmail.com>
2022-11-22 17:11:03 +0700

    nambah css

--- login.php
+++ login.php
@@ -5,13 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>
-        .kotak{
-            width: 500px;
-            height: 500px;
-            background-color: #ffffff;
-        }
-    </style>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body style="background-color: black;">
     <div class="kotak"></div>
